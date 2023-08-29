efde15dd0ec6b4a0150001c1cc7363cc77b797be
Max <fedukhinm@mail.ru>
2023-08-29 15:25:54 +0300

    translated to russian

--- src/App.js
+++ src/App.js
@@ -71,9 +71,9 @@ export default function Game() {
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
-      description = 'Go to move #' + move;
+      description = 'Вернутся к ходу #' + move;
     } else {
-      description = 'Go to game start';
+      description = 'Рестарт';
     }
     return (
       <li key={move}>
